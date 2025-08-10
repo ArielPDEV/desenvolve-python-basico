420ab2e1eefdfdf7dd966edd0c4855d5b4c7d72f
ArielPDEV <arielvieira.souzapdita912@pditabira.com>
2025-08-10 09:28:57 -0300

    Add files via upload

---
+++ Modulo3/aula2_questao1.py
@@ -0,0 +1,6 @@
+num1 = 5
+num2 = 2
+print("Variavel 1:", (num1), (type(num1)))
+print("Variavel 2:", (num2), (type(num2)))
+num3 = num1/num2
+print("Variavel 3:", (num3), (type(num3)))
---
+++ Modulo3/aula2_questao3.py
@@ -0,0 +1,7 @@
+v1 = 10
+v2 = 20
+m1 = 2
+v1 = v1 * m1
+v2 = v2 / m1
+print(v1)
+print(v2)
